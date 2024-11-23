e7ebe4c63adaa645707f2fe8e422500ffa48f6fc
manig0612 <manikandang0612@gmail.com>
2024-11-23 20:29:29 +0530

    database

---
+++ src/test/java/com/sqlfetch/FetchSql.java
@@ -0,0 +1,25 @@
+package com.sqlfetch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class FetchSql {
+	
+	public static void main(String[] args) throws SQLException {
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys", "root", "mani@sql9176");
+		Statement st = con.createStatement();
+		ResultSet exquery = st.executeQuery("select * from staffdeatails");
+		
+		while (exquery.next()) {
+			
+			String myfirst = exquery.getString(1);
+			System.out.println(myfirst);
+		}
+		
+		
+	}
+
+}
